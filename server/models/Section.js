947243eaa07fffeb71496ed1efacb4e69caaf19e
Yatrik09 <yatrikpatel1331@gmail.com>
2025-04-12 12:25:00 +0530

    Remove client and server folders from repository

--- server/models/Section.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const sectionSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    city: { type: mongoose.Schema.Types.ObjectId, ref: "City" }, // 🔗 Belongs to City
-    places: [{ type: mongoose.Schema.Types.ObjectId, ref: "Place" }] // 🔗 Connects to Places
-});
-
-module.exports = mongoose.model("Section", sectionSchema);
